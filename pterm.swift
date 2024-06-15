ea8eb6df0bf193f305d78f85fb842fa6f988f4fa
JoeyShapiro <joeyashapiro@gmail.com>
2024-06-14 20:14:58 -0500

    check for colors

--- pterm.swift
+++ pterm.swift
@@ -12,10 +12,17 @@ func runBash() {
 
     var master: Int32 = 0
     var slave: Int32 = 0
-    let result = openpty(&master, &slave, nil, nil, nil)
+    var winp: winsize = winsize()
+    
+    let result = openpty(&master, &slave, nil, nil, &winp)
     if result != 0 {
         fatalError("Error creating pseudo-terminal: \(result)")
     }
+    
+    var term: termios = termios()
+    tcgetattr(master, &term)
+    term.c_lflag &= ~UInt(ECHO | ECHOCTL)
+    tcsetattr(master, TCSAFLUSH, &term)
     
     posix_spawn_file_actions_adddup2(&fileActions, slave, STDIN_FILENO)
     posix_spawn_file_actions_adddup2(&fileActions, slave, STDOUT_FILENO)
@@ -38,16 +45,31 @@ func runBash() {
 
     close(slave)
 
+    // Read output from the spawned process
+    var outputData = Data()
+    let bufferSize = 1024
+    var buffer = [UInt8](repeating: 0, count: bufferSize)
+    
+    var bytesRead = read(master, &buffer, bufferSize)
+    print("--------- bytesRead: \(bytesRead)")
+    if let output = String(data: outputData, encoding: .utf8) {
+        print("Output from Bash:\n\(output)")
+    } else {
+        print("Error decoding output data")
+    }
+
     // Write commands to the pseudo-terminal
     let command1 = "echo 'Hello, World!'\n"
     let command2 = "ls -l\n"
     let command3 = "exit\n"
     let command4 = "python3 -c 'import sys; print(sys.stdout.isatty())'\n"
+    let command5 = "printf 'Colors: \\033[31mRed\\033[0m \\033[32mGreen\\033[0m \\033[34mBlue\\033[0m \\033[33mYellow\\033[0m \\033[35mMagenta\\033[0m \\033[36mCyan\\033[0m\n'"
     
     let commandData1 = command1.data(using: .utf8)!
     let commandData2 = command2.data(using: .utf8)!
     let commandData3 = command3.data(using: .utf8)!
     let commandData4 = command4.data(using: .utf8)!
+    let commandData5 = command5.data(using: .utf8)!
     
     commandData1.withUnsafeBytes { (ptr: UnsafeRawBufferPointer) in
         let n = write(master, ptr.baseAddress!, ptr.count)
@@ -64,31 +86,40 @@ func runBash() {
         print("wrote \(n) bytes")
     }
 
+    commandData5.withUnsafeBytes { (ptr: UnsafeRawBufferPointer) in
+        let n = write(master, ptr.baseAddress!, ptr.count)
+        print("wrote \(n) bytes")
+    }
+
+    outputData.removeAll()
+    bytesRead = read(master, &buffer, bufferSize)
+    outputData.append(buffer, count: bytesRead)
+
     commandData3.withUnsafeBytes { (ptr: UnsafeRawBufferPointer) in
         let n = write(master, ptr.baseAddress!, ptr.count)
         print("wrote \(n) bytes")
     }
 
-    // Read output from the spawned process
-    var outputData = Data()
-    let bufferSize = 1024
-    var buffer = [UInt8](repeating: 0, count: bufferSize)
-    
-    var bytesRead = read(master, &buffer, bufferSize)
-    print("--------- bytesRead: \(bytesRead)")
-    if let output = String(data: outputData, encoding: .utf8) {
-        print("Output from Bash:\n\(output)")
-    } else {
-        print("Error decoding output data")
-    }
     while bytesRead > 0 {
         outputData.removeAll()
         bytesRead = read(master, &buffer, bufferSize)
         outputData.append(buffer, count: bytesRead)
         print("-------- bytesRead: \(bytesRead)")
 
+        // check if the buffer contains \033
+        for i in 0..<bytesRead {
+            if buffer[i] == 0x1b {
+                print("buffer: \(buffer)")
+                break
+            }
+        }
+
         if let output = String(data: outputData, encoding: .utf8) {
             print("Output from Bash:\n\(output)")
+            if output.contains("exit") {
+                print("exit command found")
+                break
+            }
         } else {
             print("Error decoding output data")
         }
