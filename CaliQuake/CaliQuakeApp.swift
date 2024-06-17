0a68b84318a1d2e933905ff0bc75819bee7ad4fa
JoeyShapiro <joeyashapiro@gmail.com>
2024-06-17 16:13:05 -0500

    switched to data for normal indexing

--- CaliQuake/CaliQuakeApp.swift
+++ CaliQuake/CaliQuakeApp.swift
@@ -112,44 +112,37 @@ struct CaliQuakeApp: App {
     func keepReading() async {
         var (data, n) = pty!.read()
         while n > 0 {
-            if let output = String(data: data, encoding: .utf8) {
-                text += parse(output)
-            }
+            text += parse(data)
             (data, n) = pty!.read()
             print("read \(n)")
         }
     }
     
-    func parse(_ stdout: String) -> [AnsiChar] {
+    func parse(_ stdout: Data) -> [AnsiChar] {
         var isEsc = false
         var isMeta = false
         var parsed: [AnsiChar] = []
-        let esc = Character(Unicode.Scalar(0o33))
-        let bel = Character(Unicode.Scalar(0o7))
+        let esc = 0o33
+        let bel = 0o7
         var row = 0
         var col = 0
         var curColor = NSColor.white
         
-        for idx in (0...stdout.count-1) {
-            let i = stdout.index(stdout.startIndex, offsetBy: idx)
+        for i in (0...stdout.count-1) {
             if stdout[i] == esc {
                 isEsc = true
                 // good enough, until i do real parsing on whole thing
                 // esc [ 6 9 7 ;
                 //   0 1 2 3 4 5
-                // ??
-                let i2 = stdout.index(i, offsetBy: 2)
-                let i3 = stdout.index(i, offsetBy: 3)
-                let i4 = stdout.index(i, offsetBy: 4)
-                isMeta = stdout[i2] == "6" && stdout[i3] == "9" && stdout[i4] == "7"
-                
-                
+                isMeta = stdout[i+2] == 54 /* 6 */ &&
+                    stdout[i+3] == 57 /* 9 */ &&
+                    stdout[i+4] == 55 /* 7 */
             }
             
             if !isEsc {
-                parsed.append(AnsiChar(char: stdout[i], fg: curColor, x: col, y: row))
+                parsed.append(AnsiChar(char: Character(Unicode.Scalar(stdout[i])), fg: curColor, x: col, y: row))
                 col += 1
-                if stdout[i] == "\n" {
+                if stdout[i] == 0xa /* \n */ {
                     row += 1 // carriage return
                     col = 0  // line feed
                              // :P
@@ -163,7 +156,7 @@ struct CaliQuakeApp: App {
                 isEsc = false
                 isMeta = false
             }
-            if isEsc && !isMeta && (stdout[i] == "m" || stdout[i] == "h") {
+            if isEsc && !isMeta && (stdout[i] == 109 /* m */ || stdout[i] == 104 /* h */) {
                 isEsc = false
             }
         }
