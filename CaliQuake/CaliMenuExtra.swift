987ef8c50df33db353746e2bad48a981de7e7005
JoeyShapiro <joeyashapiro@gmail.com>
2024-07-13 12:47:02 -0500

    added cursor

--- CaliQuake/CaliMenuExtra.swift
+++ CaliQuake/CaliMenuExtra.swift
@@ -20,7 +20,9 @@ struct CaliMenuExtra: View {
     }
     
     var body: some View {
-        Text("")
-        Image(self.grid.makeImage(size: self.size, pointSize: 12)!, scale: 2.0, label: Text("test"))
+        ZStack {
+            Image(self.grid.makeImage(size: self.size, pointSize: 12)!, scale: 2.0, label: Text("text"))
+            Image(self.grid.makeCursor(size: self.size)!, scale: 2.0, label: Text("cursor"))
+        }
     }
 }
