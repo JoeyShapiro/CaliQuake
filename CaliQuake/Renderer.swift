a4741f61b5b0ee198a112a16681912e574774e31
JoeyShapiro <joeyashapiro@gmail.com>
2024-07-13 12:11:29 -0500

    checkpoint: trying to get metal image

--- CaliQuake/Renderer.swift
+++ CaliQuake/Renderer.swift
@@ -85,6 +85,7 @@ class Renderer: NSObject {
     }
 
     func draw(in view: MTKView) {
+        view.framebufferOnly = false
         if isDirty {
             // NSFont.monospacedSystemFont(ofSize: 12, weight: NSFont.Weight(rawValue: 0.1))
 //            guard let font = NSFont(name: "SFMono-Regular", size: 12) else {
@@ -138,7 +139,26 @@ class Renderer: NSObject {
 
         commandBuffer.present(drawable)
         commandBuffer.commit()
+        commandBuffer.waitUntilCompleted()
+        
+        
+//        let copy = drawable.texture.toImage()
+        
+//        // save CGImage as a png
+//         let url = URL(fileURLWithPath: "/Users/oniichan/Downloads/render.png")
+//         let cicontext = CIContext()
+//         let ciimage = CIImage(cgImage: copy!)
+//         do {
+//             try cicontext.writePNGRepresentation(of: ciimage, to: url, format: .RGBA8, colorSpace: ciimage.colorSpace!)
+//             print("done")
+//         } catch {
+//        
+//        
+//         }
+        view.framebufferOnly = true
     }
+    
+    
     
     func convertCGImageToData(_ cgImage: CGImage, format: CFString = kUTTypePNG, quality: CGFloat = 1.0) -> Data? {
         let data = NSMutableData()
@@ -156,5 +176,38 @@ class Renderer: NSObject {
         }
         
         return data as Data
+    }
+}
+
+extension MTLTexture {
+    
+    func bytes() -> UnsafeMutableRawPointer {
+        let width = self.width
+        let height   = self.height
+        let rowBytes = self.width * 4
+        let p = malloc(width * height * 4)
+        
+        self.getBytes(p!, bytesPerRow: rowBytes, from: MTLRegionMake2D(0, 0, width, height), mipmapLevel: 0)
+        
+        return p!
+    }
+    
+    func toImage() -> CGImage? {
+        let p = bytes()
+        
+        let pColorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        let rawBitmapInfo = CGImageAlphaInfo.noneSkipFirst.rawValue | CGBitmapInfo.byteOrder32Little.rawValue
+        let bitmapInfo:CGBitmapInfo = CGBitmapInfo(rawValue: rawBitmapInfo)
+        
+        let selftureSize = self.width * self.height * 4
+        let rowBytes = self.width * 4
+        let releaseMaskImagePixelData: CGDataProviderReleaseDataCallback = { (info: UnsafeMutableRawPointer?, data: UnsafeRawPointer, size: Int) -> () in
+            return
+        }
+        let provider = CGDataProvider(dataInfo: nil, data: p, size: selftureSize, releaseData: releaseMaskImagePixelData)
+        let cgImageRef = CGImage(width: self.width, height: self.height, bitsPerComponent: 8, bitsPerPixel: 32, bytesPerRow: rowBytes, space: pColorSpace, bitmapInfo: bitmapInfo, provider: provider!, decode: nil, shouldInterpolate: true, intent: CGColorRenderingIntent.defaultIntent)!
+        
+        return cgImageRef
     }
 }
